1dbaedba1d6b37c7021e6db0f17ad600d835edd5
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-03 15:12:04 -0400

    Create implementation of Practice Interface

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -2,6 +2,16 @@ package ca.jrvs.apps.practice;
 
 class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World");
+    RegexPractice rp = new RegexPractice();
+
+    // Testing Regex
+    String emptyLine = "";
+    System.out.println("Matching empty line: " + rp.isEmptyLine(emptyLine));
+
+    String jpgFile = "Star.jpeg";
+    System.out.println("Matching jpg file: " + rp.matchJpeg(jpgFile));
+
+    String ip = "255.0.0.0";
+    System.out.println("Matching ip address: " + rp.matchIp(ip));
   }
 }
---
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/RegexExc.java
@@ -0,0 +1,9 @@
+package ca.jrvs.apps.practice;
+
+public interface RegexExc {
+  public boolean matchJpeg(String filename);
+
+  public boolean matchIp(String ip);
+
+  public boolean isEmptyLine(String line);
+}
