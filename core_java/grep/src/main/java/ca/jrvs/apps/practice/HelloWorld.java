4ff814a5588a31e67040a1246a18a32d9dbe7630
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-09 16:38:08 -0400

    Practce HashMap implementation

--- core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
+++ core_java/grep/src/main/java/ca/jrvs/apps/practice/HelloWorld.java
@@ -1,7 +1,42 @@
 package ca.jrvs.apps.practice;
 
+import ca.jrvs.apps.practice.dataStructure.map.Employee;
+import ca.jrvs.apps.practice.dataStructure.map.JHashMap;
+import ca.jrvs.apps.practice.dataStructure.map.JMap;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 class HelloWorld {
   public static void main(String[] args) {
+
+    //testingRegex(args);
+    hashMapPractice(args);
+  }
+
+  private static void hashMapPractice(String[] args) {
+    JMap<Employee, List<String>> empStrMap = new JHashMap<>();
+
+    //Amy
+    Employee amy = new Employee(1, "Amy", 25, 45000);
+    List<String> amyPreviousCompanies = Arrays.asList("TDL", "RBC", "CIBC");
+    empStrMap.put(amy, amyPreviousCompanies);
+
+    //Bob
+    Employee bob = new Employee(1, "Bob", 18, 40000);
+    List<String> bobPreviousCompanies = Arrays.asList("A&W", "Superstore");
+    empStrMap.put(bob, bobPreviousCompanies);
+
+    System.out.println(empStrMap.get(bob));
+
+//    for (Employee emp : empStrMap.keySet()){
+//      System.out.println("Hashcode: " + emp.hashCode());
+//      System.out.println("Value: " + empStrMap.get(emp).toString());
+//      System.out.println("-----------");
+//    }
+  }
+  private static void testingRegex(String[] args) {
     RegexPractice rp = new RegexPractice();
 
     // Testing Regex
