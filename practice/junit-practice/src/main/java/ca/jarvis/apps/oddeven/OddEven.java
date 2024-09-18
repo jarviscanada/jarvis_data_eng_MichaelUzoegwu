5e37c98af23c41d15864bf08e84ef656fb4e7eff
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-18 14:47:58 -0400

    Practice test creation with IDE

---
+++ practice/junit-practice/src/main/java/ca/jarvis/apps/oddeven/OddEven.java
@@ -0,0 +1,19 @@
+package ca.jarvis.apps.oddeven;
+
+public class OddEven {
+  /**
+   * Big-O: O(1)
+   * Justification: it's an arithmetic operation
+   */
+  public String oddEvenMod(int i){
+    return i % 2 == 0 ? "even" : "odd";
+  }
+
+  /**
+   * Big-O:
+   * Justification:
+   */
+  public String oddEvenBit(int i){
+    return null;
+  }
+}
