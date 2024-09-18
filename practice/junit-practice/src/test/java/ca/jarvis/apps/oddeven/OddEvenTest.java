7b783308d2ecc1a6add47f53e4457aa14f0bf17d
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-18 14:47:58 -0400

    Practice test creation with IDE

---
+++ practice/junit-practice/src/test/java/ca/jarvis/apps/oddeven/OddEvenTest.java
@@ -0,0 +1,18 @@
+package ca.jarvis.apps.oddeven;
+
+import static org.junit.Assert.*;
+
+public class OddEvenTest {
+
+  @org.junit.Before
+  public void setUp() throws Exception {
+  }
+
+  @org.junit.Test
+  public void oddEvenMod() {
+  }
+
+  @org.junit.Test
+  public void oddEvenBit() {
+  }
+}
