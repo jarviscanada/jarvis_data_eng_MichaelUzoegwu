a5319101ffe98b4c1032a494367114b1266db6d4
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-30 14:17:43 -0400

    Create Quote Service unit tests

---
+++ core_java/jdbc/src/test/java/ca/jrvs/apps/stockquote/QuoteService_UnitTest.java
@@ -0,0 +1,70 @@
+package ca.jrvs.apps.stockquote;
+
+import ca.jrvs.apps.stockquote.dto.Quote;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+import java.util.Optional;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.*;
+
+@RunWith(MockitoJUnitRunner.class)
+public class QuoteService_UnitTest {
+
+  QuoteService quoteService;
+
+  @Mock
+  QuoteHttpHelper helperMock;
+
+  @Before
+  public void setUp() throws Exception {
+    quoteService = new QuoteService(helperMock);
+  }
+
+  @Test
+  public void fetchQuoteDataFromAPI() {
+    final String ticker = "AAPL";
+    Quote helperQuote = createQuote(ticker);
+    when(helperMock.fetchQuoteInfo(ticker)).thenReturn(helperQuote);
+
+    Optional<Quote> fetchedQuoteOpt = quoteService.fetchQuoteDataFromAPI(ticker);
+    assertEquals("Expected fetched quote to match quote retrieved from http helper. Instead returned quote was " +
+            "different.", helperQuote, fetchedQuoteOpt.orElseThrow()
+    );
+  }
+
+  @Test
+  public void fetchQuoteDataFromAPINonExistent() {
+    final String ticker = "NON_EXISTENT";
+    when(helperMock.fetchQuoteInfo(ticker)).thenThrow(new IllegalArgumentException(
+            String.format("Could not find any data for provided symbol '%s'", ticker)
+    ));
+
+    Optional<Quote> fetchedQuoteOpt = quoteService.fetchQuoteDataFromAPI(ticker);
+
+    assertTrue("Expected fetched quote to be empty. Instead quote had a value",
+            fetchedQuoteOpt.isEmpty());
+  }
+
+  private Quote createQuote(String ticker) {
+    Quote quote = new Quote();
+    quote.setTicker(ticker);
+    quote.setOpen(145.10);
+    quote.setHigh(147.20);
+    quote.setLow(144.50);
+    quote.setPrice(146.00);
+    quote.setVolume(1000000);
+    quote.setLatestTradingDay(new Date(10921048));
+    quote.setPreviousClose(145.50);
+    quote.setChange(0.50);
+    quote.setChangePercent("0.34%");
+    quote.setTimestamp(new Timestamp(System.currentTimeMillis()));
+    return quote;
+  }
+}
