8e3250cddf3ecbd7fb7472380ddc814fa3caaf84
Michael Uzoegwu <41979390+micuzo@users.noreply.github.com>
2024-09-30 14:17:25 -0400

    Create Quote and Position services

--- core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/App.java
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/App.java
@@ -16,61 +16,7 @@ public class App {
     } else if (args.length > 1) {
       System.out.println("Incorrect usage, too many arguments.");
     }
-
     apiKey = args[0];
-
-    // Connect to DB
-    try (Connection connection = DBConnector.getConnection()) {
-      // Get Quote via API
-      QuoteHttpHelper helper = new QuoteHttpHelper();
-      String[] stockSymbols = {
-              "AAPL",  // Apple Inc.
-              "GOOGL", // Alphabet Inc. (Google)
-              "AMZN",  // Amazon.com Inc.
-              "MSFT",  // Microsoft Corporation
-              "TSLA",  // Tesla Inc.
-              "FB",    // Meta Platforms Inc. (formerly Facebook)
-              "NFLX",  // Netflix Inc.
-              "NVDA",  // NVIDIA Corporation
-              "BRK.A", // Berkshire Hathaway Inc.
-              "JPM"    // JPMorgan Chase & Co.
-      };
-
-      QuoteDao qd = new QuoteDao(connection);
-
-      //Pop DB
-//      for (int i = 0; i < 5; i++) {
-//        qd.save(helper.fetchQuoteInfo(stockSymbols[i]));
-//      }
-
-      // DAO -- Quote
-      //qd.deleteAll();
-      //qd.deleteById("AAPL");
-      //qd.save(quote);
-      //quote = qd.findById("AAPL").get();
-      //System.out.println(quote);
-      //Iterable<Quote> quotes = qd.findAll();
-      //quotes.forEach(System.out::println);
-
-
-      PositionDao pd = new PositionDao(connection);
-
-//      Position myPos = new Position();
-//      myPos.setTicker("AMZN");
-//      myPos.setNumOfShares(80);
-//      myPos.setValuePaid(70);
-//      pd.save(myPos);
-
-//      pd.deleteById("MSFT");
-
-//      pd.deleteAll();
-//
-//      Iterable<Position> allPos = pd.findAll();
-//      allPos.forEach(System.out::println);
-
-    } catch (SQLException e) {
-      e.printStackTrace();
-    }
   }
 
   public static String getApiKey() {
---
+++ core_java/jdbc/src/main/java/ca/jrvs/apps/stockquote/QuoteService.java
@@ -0,0 +1,30 @@
+package ca.jrvs.apps.stockquote;
+
+import ca.jrvs.apps.stockquote.dao.QuoteDao;
+import ca.jrvs.apps.stockquote.dto.Quote;
+
+import java.util.Optional;
+
+public class QuoteService {
+  final private QuoteHttpHelper httpHelper;
+  private QuoteDao dao;
+
+  public QuoteService(QuoteHttpHelper httpHelper) {
+    this.httpHelper = httpHelper;
+  }
+
+  /**
+   * Fetches latest quote data from endpoint
+   *
+   * @param ticker quote's symbol
+   * @return Latest quote information or empty optional if ticker symbol not found
+   */
+  public Optional<Quote> fetchQuoteDataFromAPI(String ticker) {
+    try {
+      Quote quote = httpHelper.fetchQuoteInfo(ticker);
+      return Optional.of(quote);
+    } catch (IllegalArgumentException e) {
+      return Optional.empty();
+    }
+  }
+}
